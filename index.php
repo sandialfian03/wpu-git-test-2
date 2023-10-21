898d9825cd61a2265d5acd7659804fc5e3e5696b
sandi alfian <sandialfian62@gmail.com>
2023-10-22 01:10:31 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>hello word!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At sint facilis illum sequi recusandae, autem quibusdam dignissimos eaque nostrum quos omnis quas soluta eum. Possimus nobis numquam mollitia blanditiis eveniet?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
